0b24a40cfacc5f7d6d2762dd55025584bfb49e8e
matthew philyaw <matthew.philyaw@gmail.com>
2015-12-01 20:50:29 +0000

    actually adding the dev.exs file... whoops...

---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+# Elixir Ale is just under I2c
+config :motor_hat, i2c: I2c
+
